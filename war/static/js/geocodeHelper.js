b311c20bdf5f873986661bbfae418e4c2f7afe48
Eric Beach <>
2015-05-16 12:20:19 -0400

    Make Geocode location response parsing dynamic -- no longer depending upon variable names that are hardcoded.

--- war/static/js/geocodeHelper.js
+++ war/static/js/geocodeHelper.js
@@ -23,44 +23,7 @@ GeocodeHelper.prototype.geocodeAddress = function(address) {
   var promise = new Promise(function(resolve, reject) {
     this_.geocoder_.geocode({ 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-        // Note: At times even when you enter a very specific address, Google
-        // returns 2 or 3 possible results. The proper way to handle this is to
-        // let the user decide which address. Until then, arbitrarily setting
-        // cutoff at 4 meaning if Google returns 4 or more addresses, we tell
-        // the user to be more specific. If Google returns 3 or less, we
-        // simply use the first result.
-        if (results.length >= 4) {
-          reject('Multiple possible locations found. Please enter a more ' +
-              'specific address. For example: 1600 Pennsylvania Ave NW, ' +
-              'Washington, DC, 20500');
-        } else if (results.length == 0) {
-          reject('Unable to find any locations cooresponding to that address.');
-        } else if (typeof results[0].geometry.location.lat == 'number' &&
-            typeof results[0].geometry.location.lng == 'number') {
-          var coordinates = {
-            latitude: results[0].geometry.location.lat,
-            longitude: results[0].geometry.location.lng
-          };
-          resolve(coordinates);
-        } else if (typeof results[0].geometry.location.D == 'number' &&
-                   typeof results[0].geometry.location.k == 'number') {
-          var coordinates = {
-            latitude: results[0].geometry.location.k,
-            longitude: results[0].geometry.location.D
-          };
-          resolve(coordinates);
-        } else if (typeof results[0].geometry.location.A == 'number' &&
-            typeof results[0].geometry.location.F == 'number') {
-          var coordinates = {
-            latitude: results[0].geometry.location.A,
-            longitude: results[0].geometry.location.F
-          };
-          resolve(coordinates);
-        } else {
-          // Query executed successfully, but could not pinpoint one address.
-          reject('Unable to identify a single specific point on the map.' +
-              ' Try a more specific address.');
-        }
+        this_.getLatLongFromMapsAPIResults_(results, resolve, reject);
       } else {
         reject('Geocode was not successful for the following reason: ' +
             status);
@@ -68,4 +31,50 @@ GeocodeHelper.prototype.geocodeAddress = function(address) {
     });
   });
   return promise;
+};
+
+
+/**
+ * @param {Object} results
+ * @param {*} resolve
+ * @param {*} reject
+ * @private
+ */
+GeocodeHelper.prototype.getLatLongFromMapsAPIResults_ = function(results,
+    resolve, reject) {
+  // Note: At times even when you enter a very specific address, Google
+  // returns 2 or 3 possible results. The proper way to handle this is to
+  // let the user decide which address. Until then, arbitrarily setting
+  // cutoff at 4 meaning if Google returns 4 or more addresses, we tell
+  // the user to be more specific. If Google returns 3 or less, we
+  // simply use the first result.
+  if (results.length >= 4) {
+    reject('Multiple possible locations found. Please enter a more ' +
+        'specific address. For example: 1600 Pennsylvania Ave NW, ' +
+        'Washington, DC, 20500');
+  } else if (results.length == 0) {
+    reject('Unable to find any locations cooresponding to that address.');
+  } else if (typeof results[0].geometry.location == 'object' &&
+             Object.keys(results[0].geometry.location).length == 2) {
+    // A successful regulst exists and there are lat + long properties.
+    var geocodedLocationObj = results[0].geometry.location;
+    var geocodedLocationObjKeys = Object.keys(geocodedLocationObj);
+
+    if (typeof geocodedLocationObj[geocodedLocationObjKeys[0]] == 'number' &&
+        typeof geocodedLocationObj[geocodedLocationObjKeys[1]] == 'number') {
+      var coordinates = {
+        latitude: geocodedLocationObj[geocodedLocationObjKeys[0]],
+        longitude: geocodedLocationObj[geocodedLocationObjKeys[1]]
+      };
+      resolve(coordinates);
+    } else {
+      // Query executed successfully, but data appears malformed.
+      reject('Unable to identify a single specific point on the map.' +
+          ' If this persists, please contact the administrator.');
+    }
+  } else {
+    // Query executed successfully, but could not pinpoint one address.
+    reject('Unable to identify a single specific point on the map.' +
+        ' Try a more specific address.');
+  }
 };
